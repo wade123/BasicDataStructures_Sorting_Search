60aab32826736ab802a0a813258a18081bd900d0
yu_win7 <yutingshan@gmail.com>
2015-10-04 18:20:16 -0500

    add readme.md

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,25 +2,25 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void bubbleSort(int[] nums) {
-    	int length = nums.length;
-    	for (int i = 0; i < length - 1; i++) {
-    		for (int j = 0; j < length - 1 - i; j++) {
-    			if (nums[j] > nums[j + 1]) {
-    				swap(nums, j, j + 1);
-    			}
-    		}
-    	}
+        int length = nums.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
+                }
+            }
+        }
     }
 
     private static void swap(int[] nums, int i, int j) {
-    	int temp = nums[i];
-    	nums[i] = nums[j];
-    	nums[j] = temp;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
     public static void main(String[] args) {
-    	int[] nums = {3, 5, 4, 1, 2};
-    	BubbleSort.bubbleSort(nums);
-    	System.out.println(Arrays.toString(nums));
+        int[] nums = {3, 5, 4, 1, 2};
+        BubbleSort.bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
---
+++ readme.md
@@ -0,0 +1,13 @@
+Basic Data Structures, Sorting and Searching Algorithms
+=======
+
+Java Implementations
+-----------
+ 
+### Oct. 04 2015
+ 
+Including:
+
+  * Data Structures: ArrayList, Heap, LinkedList, Queue, Stack, HashMap, StringBuilder, etc.
+  * Searching: Binary Search
+  * Sorting: QuickSort, MergeSort, TimSort, HeapSort, BubbleSort, InsertionSort, SelectionSort, ShellSort, BucketSort, RadixSort
